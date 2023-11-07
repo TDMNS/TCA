6e64e249514c4d231f193e0d8bf8530d6fb02a77
Oleg <773o@mail.ru>
2023-11-07 20:39:06 +0700

    Initial Commit

---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by Oleg Kolbasa on 07.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
